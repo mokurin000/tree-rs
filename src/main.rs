fda488a08e3422a9a5b44d5a7a68f903c87f8ff5
mokurin000 <1348292515a@gmail.com>
2025-07-16 00:38:53 +0800

    chore: real parallism for performance

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 use std::{error::Error, fmt::Write, fs::read_dir, path::Path};
 
 use compio::{fs, runtime::spawn_blocking};
+use futures_util::{StreamExt, stream};
 
 #[compio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut output = String::new();
-    visit_dirs(".", &mut output).await?;
+    let cpu_count = num_cpus::get();
+    visit_dirs(".", &mut output, cpu_count).await?;
 
     _ = fs::write("./tree.csv", output.replace("\\", "/")).await;
     Ok(())
 }
 
-async fn visit_dirs(path: impl AsRef<Path>, out: &mut impl Write) -> Result<(), Box<dyn Error>> {
+async fn visit_dirs(
+    path: impl AsRef<Path>,
+    out: &mut impl Write,
+    cpu_count: usize,
+) -> Result<(), Box<dyn Error>> {
     let path = path.as_ref();
     let path_str = path.to_string_lossy();
 
@@ -32,11 +38,17 @@ async fn visit_dirs(path: impl AsRef<Path>, out: &mut impl Write) -> Result<(), 
             .await
             .unwrap()?
             .flatten()
-            .map(|e| e.path())
-            .collect::<Vec<_>>();
-        for path in results {
-            Box::pin(visit_dirs(path, out)).await?;
-        }
+            .map(|e| e.path());
+        let new = stream::iter(results)
+            .map(async |path| {
+                let mut out = String::new();
+                _ = visit_dirs(path, &mut out, cpu_count).await;
+                out
+            })
+            .buffer_unordered(cpu_count)
+            .collect::<String>()
+            .await;
+        out.write_str(&new)?;
     } else {
         let file_size = meta.len();
         out.write_fmt(format_args!("{path_str},{file_size}\n"))?;
