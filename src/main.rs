91936c717195afb9f618cf6230e4cc99e85383dc
mokurin000 <1348292515a@gmail.com>
2025-07-16 00:15:11 +0800

    fix: read directory

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use std::{error::Error, fmt::Write, path::Path};
+use std::{error::Error, fmt::Write, fs::read_dir, path::Path};
 
-use compio::fs;
+use compio::{fs, runtime::spawn_blocking};
 
 #[compio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut output = String::new();
     visit_dirs(".", &mut output).await?;
 
-    _ = fs::write("./tree.csv", output).await;
+    _ = fs::write("./tree.csv", output.replace("\\", "/")).await;
     Ok(())
 }
 
-async fn visit_dirs(path: impl AsRef<Path>, mut out: impl Write) -> Result<(), Box<dyn Error>> {
+async fn visit_dirs(path: impl AsRef<Path>, out: &mut impl Write) -> Result<(), Box<dyn Error>> {
     let path = path.as_ref();
     let path_str = path.to_string_lossy();
 
@@ -27,7 +27,16 @@ async fn visit_dirs(path: impl AsRef<Path>, mut out: impl Write) -> Result<(), B
     };
 
     if meta.is_dir() {
-        Box::pin(visit_dirs(path, out)).await?;
+        let path = path.to_path_buf();
+        let results = spawn_blocking(move || read_dir(path))
+            .await
+            .unwrap()?
+            .flatten()
+            .map(|e| e.path())
+            .collect::<Vec<_>>();
+        for path in results {
+            Box::pin(visit_dirs(path, out)).await?;
+        }
     } else {
         let file_size = meta.len();
         out.write_fmt(format_args!("{path_str},{file_size}\n"))?;
